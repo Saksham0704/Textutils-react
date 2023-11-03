3599ae153908d8f5437dbc65071c2815464cb2e7
Saksham Agarwal <sakshamagarwal1616@gmail.com>
2023-11-03 21:40:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Signup from './components/Signup';
+// import { RouterProvider,createBrowserRouter } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
+
+// const MainRouter = createBrowserRouter([
+//   {path:'/',Component:Login},
+//   {path:'/login',Component:Login},
+//   {path:'/signup',Component:Signup},
+//   {path:'/dashboard',Component:Dashboard}
+// ])
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+{/* <RouterProvider router={MainRouter} /> */}
+{/* <Login />
+<Signup />
+<Dashboard /> */}
+<Login/>
+<Dashboard/>
+<Signup/>
+</>
   );
 }
 
